4ee5f3569d0475a5fb9eeed0ca4d11f82e8f0c5f
suneelpamujula035 <suneelpamujula@gmail.com>
2025-05-26 12:06:27 +0530

    jax-rs config changed

--- src/main/java/com/student/config/RestConfig.java
+++ src/main/java/com/student/config/RestConfig.java
@@ -1,13 +1,14 @@
-//package com.student.config;
-//
-//import com.student.resource.StudentResource;
-//import org.glassfish.jersey.server.ResourceConfig;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class RestConfig extends ResourceConfig {
-//
-//    public RestConfig(Class<?>... classes) {
-//        register(StudentResource.class);
-//    }
-//}
+package com.student.config;
+
+import com.student.resource.StudentResource;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RestConfig extends ResourceConfig {
+
+    public RestConfig(Class<?>... classes) {
+        register(StudentResource.class);
+        setApplicationName("api");
+    }
+}
--- src/main/java/com/student/resource/StudentResource.java
+++ src/main/java/com/student/resource/StudentResource.java
@@ -1,30 +1,28 @@
-//package com.student.resource;
-//
-//import com.student.entity.Student;
-//import com.student.repository.StudentRepository;
-//
-//import jakarta.ws.rs.Consumes;
-//import jakarta.ws.rs.GET;
-//import jakarta.ws.rs.Path;
-//import jakarta.ws.rs.Produces;
-//import jakarta.ws.rs.core.MediaType;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//
-//import java.util.List;
-//
-//@Path("/api")
-//public class StudentResource {
-//
-//    @Autowired
-//    private StudentRepository studentRepository;
-//
-//    @GET
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    @Produces(MediaType.APPLICATION_JSON)
-//    @Path("/getAllStudents")
-//    public List<Student> getAllStudents()
-//    {
-//        return studentRepository.findAll();
-//    }
-//}
+package com.student.resource;
+
+import com.student.entity.Student;
+import com.student.repository.StudentRepository;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Path("/students")
+public class StudentResource {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/getAllStudents")
+    public List<Student> getAllStudents()
+    {
+        return studentRepository.findAll();
+    }
+}
