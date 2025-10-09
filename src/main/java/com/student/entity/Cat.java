cd96f4b5f1e2e177a3c6c266d7d3c1b5a3920584
suneelpamujula035 <suneelpamujula@gmail.com>
2025-10-09 14:54:06 +0530

    cat added

---
+++ src/main/java/com/student/entity/Cat.java
@@ -0,0 +1,27 @@
+package com.student.entity;
+
+public class Cat {
+    private  String name;
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public Cat() {
+    }
+}
