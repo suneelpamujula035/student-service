9adb399abf2c50c4cd89c5b6a39f930276cfdb81
suneelpamujula035 <suneelpamujula@gmail.com>
2025-10-09 15:29:14 +0530

    car added

---
+++ src/main/java/com/student/entity/Car.java
@@ -0,0 +1,27 @@
+package com.student.entity;
+
+public class Car {
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public Car() {
+    }
+}
